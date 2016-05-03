e3e420e097f83858be12b90ec9da81a074b762a3
Martin Gladdish <martin@martingladdish.co.uk>
2016-05-03 17:07:03 +0100

    Make sure timestamp is included in the spreadsheet submission

--- src/test/scala/com/equalexperts/scaladayscompetitionform/submitToGoogleSpec.scala
+++ src/test/scala/com/equalexperts/scaladayscompetitionform/submitToGoogleSpec.scala
@@ -2,11 +2,12 @@ package com.equalexperts.scaladayscompetitionform
 
 import java.util.Locale
 
-import com.uxforms.domain.RequestInfo
-import org.scalatest.{FreeSpec, Matchers}
+import com.uxforms.domain.{FormDefinition, RequestInfo}
+import org.joda.time.{DateTimeUtils, Instant}
+import org.scalatest.{FreeSpec, Matchers, OptionValues}
 import play.api.libs.json.Json
 
-class submitToGoogleSpec extends FreeSpec with Matchers {
+class submitToGoogleSpec extends FreeSpec with Matchers with OptionValues {
 
   "convertFormData" - {
 
@@ -18,20 +19,38 @@ class submitToGoogleSpec extends FreeSpec with Matchers {
 
     "should include the client's IP from the X-Forwarded-For HTTP Header" in {
       val headers = baseHeaders :+ "X-Forwarded-For" -> "5.148.6.178"
-      val result = submitToGoogle.convertFormData(formData, MyFormDefinitionFactory.formDefinition(new Locale("en", "GB")), RequestInfo(Map.empty, headers))
+      val result = submitToGoogle.convertFormData(formData, formDef, RequestInfo(Map.empty, headers))
 
       result.columnNames should contain("xForwardedFor")
       result.data.head should contain("5.148.6.178")
     }
 
     "should leave the xForwardedFor column blank if the X-Forwarded-For HTTP header is not present" in {
-      val result = submitToGoogle.convertFormData(formData, MyFormDefinitionFactory.formDefinition(new Locale("en", "GB")), RequestInfo(Map.empty, baseHeaders))
+      val result = submitToGoogle.convertFormData(formData, formDef, RequestInfo(Map.empty, baseHeaders))
 
       result.columnNames should contain("xForwardedFor")
       result.data.head.last shouldBe ""
     }
 
+    "should include the timestamp of the submission in ISO8601 format" in {
+      withFrozenTime() { instant =>
+        val result = submitToGoogle.convertFormData(formData, formDef, RequestInfo(Map.empty, Seq.empty))
+        result.columnNames should contain("timestamp")
+        val timestampColumnIndex = formDef.flattenWidgets.length
+        result.data.head.lift(timestampColumnIndex).value shouldBe instant.toString
+      }
+    }
+
   }
+
+
+  def withFrozenTime[T](frozenTime: Instant = Instant.now)(body: Instant => T) = {
+    DateTimeUtils.setCurrentMillisFixed(frozenTime.getMillis)
+    body(frozenTime)
+    DateTimeUtils.setCurrentMillisSystem()
+  }
+
+  private def formDef: FormDefinition = MyFormDefinitionFactory.formDefinition(new Locale("en", "GB"))
 
   private val formData = Json.obj("name" -> "Bob Smith", "email" -> "bob@example.com", "phone" -> "123561823", "twitter" -> "bsmith", "github" -> "bsmith")
 
